d728f6af24b19141f6000081a474ac562af7ef31
tuanzi <tuanzi@2dfire.com>
2016-07-29 17:36:47 +0800

    优化权限控制页面跳转部分

--- design/src/main/java/com/shaw/aop/AuthInterceptor.java
+++ design/src/main/java/com/shaw/aop/AuthInterceptor.java
@@ -17,22 +17,25 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 		if (handler.getClass().isAssignableFrom(HandlerMethod.class)) {
 			RolePassport rolePassport = ((HandlerMethod) handler).getMethodAnnotation(RolePassport.class);
+			// 判断是否是ajax请求
 			User user = null;
-			if (rolePassport == null || rolePassport.validate() == false)
+			Boolean isAjax = (request.getHeader("x-requested-with") != null
+					&& request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest"));
+			if (rolePassport == null || rolePassport.validate() == false) {
 				return true;
-			else if ((user = (User) request.getSession().getAttribute("user")) != null) {
+			} else if ((user = (User) request.getSession().getAttribute("user")) != null) {
 				if (rolePassport.roleLv() == 1) {
 					return true;
 				}
 				if (rolePassport.roleLv() == 2) {
 					if (user.getRole() == 1) {
-						response.sendRedirect("unrole.html?redirectUrl=" + request.getRequestURI());
+						redirect(request, response, isAjax, false);
 						return false;
 					}
 				}
 				if (rolePassport.roleLv() == 3) {
 					if (user.getRole() != 3) {
-						response.sendRedirect("unrole.html?redirectUrl=" + request.getRequestURI());
+						redirect(request, response, isAjax, false);
 						return false;
 					}
 				}
@@ -41,10 +44,33 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 				Cookie cookie = new Cookie("user", null);
 				cookie.setMaxAge(0);
 				response.addCookie(cookie);
-				response.sendRedirect("login.html?redirectUrl=" + request.getRequestURI());
+				redirect(request, response, isAjax, true);
+				return false;
 			}
-			return false;
 		}
 		return true;
+	}
+
+	private void redirect(HttpServletRequest request, HttpServletResponse response, Boolean isAjax, Boolean toLogin)
+			throws Exception {
+		response.setHeader("Content-type", "text/html;charset=UTF-8");
+		if (isAjax) {
+			if (toLogin) {
+				response.setHeader("sessionstatus", "sessionOut");
+				response.getWriter().write("登录已失效，将调用JS自动跳转至登录页...");
+				response.getWriter().write("<script>$(function (){window.location.replace('login.html');});</script>");
+			} else {
+				response.setHeader("sessionstatus", "sessionOut");
+				response.getWriter().write("权限不足");
+				response.getWriter().write("<script>$(function (){window.location.replace('unrole.html');});</script>");
+			}
+		} else {
+			if (toLogin) {
+				response.sendRedirect("login.html?redirectUrl=" + request.getRequestURI());
+			} else {
+				response.sendRedirect("unrole.html?redirectUrl=" + request.getRequestURI());
+			}
+		}
+
 	}
 }
